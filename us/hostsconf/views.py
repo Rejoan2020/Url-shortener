22d4829d8375de317d8f2b96dcac1b4b7dfd0f34
Md. Rejoanur Rahman Apu <rej.ash11@gmail.com>
2023-12-21 03:15:23 +0600

    vercel domain set up

--- us/hostsconf/views.py
+++ us/hostsconf/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
-DEFAULT_REDIRECT_PATH = getattr(settings,'DEFAULT_REDIRECT_PATH','http://www.tirr.com:8000')
+DEFAULT_REDIRECT_PATH = getattr(settings,'DEFAULT_REDIRECT_PATH','http://url-shortener-ashy-two.vercel.app')
 
 def wildcard_redirect(request,path=None):
     new_url = DEFAULT_REDIRECT_PATH
